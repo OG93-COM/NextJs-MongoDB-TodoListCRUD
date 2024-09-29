ad7bdd3cc7fe8d259f29e693284c433c4849c336
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-09-29 16:31:29 +0100

    BlogOG

--- blogog/components/Post.tsx
+++ blogog/components/Post.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {postsData} from '../data'
 
 const Post = () => {
+    console.log(postsData)
   return (
     <div className='mt-5'>
-
+        {postsData?.map(item => (
+            <p>{item.title}</p>
+        ))}
     </div>
   )
 }

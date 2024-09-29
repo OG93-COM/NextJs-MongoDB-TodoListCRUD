241697bd119f484928d13cce6bb45f147625779a
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-09-29 02:29:55 +0100

    BlogOG

--- blogog/app/signin/page.tsx
+++ blogog/app/signin/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
-      <h1 className='text-xl font-semibold mb-5'>Sigin Page</h1>
+      <h1 className='mb-5'>Sigin Page</h1>
       <SignInBtn/>
 
     </div>

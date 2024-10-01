9b0483a73cb141ddbcc0f9e1612e06a73c73d936
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-02 00:31:10 +0100

    BlogOG

--- blogog/app/api/posts/route.ts
+++ blogog/app/api/posts/route.ts
@@ -1,10 +1,38 @@
 import prisma from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(req: Request) {
+    const authorEmail = "contact@og93.com"
+
+    const {title, content, links, imgUrl, selectedCategory, publicId } = await req.json()
+
+    if(!title || !content) {
+        return NextResponse.json({error:"Title and Content Are Required"},{status:500})
+    }
+
     try {
-        const post = prisma.post.create()
+        const newPost = await prisma.post.create({
+            data:{
+                title, content, links, imgUrl, publicId, catName: selectedCategory, authorEmail
+            }
+        })
+        console.log("Post Created 📝")
+        return NextResponse.json(newPost)
     } catch (error) {
-        
+        return NextResponse.json({message:"Could Not Create the post ❌ "})
+    }
+}
+
+export async function GET(){
+
+    try {
+        const posts = await prisma.post.findMany(
+            {
+                include: {author: {select: {name:true}}},
+                    orderBy:{ createdAt:"desc" }
+        })
+        return NextResponse.json(posts)
+    } catch (error) {
+        return NextResponse.json({message: "Problem Get DATA Post Server ❌"}, {status:500})
     }
 }

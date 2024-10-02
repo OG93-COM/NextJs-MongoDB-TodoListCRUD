67c33ba363904b7d9f13e540340a66fb14de69f2
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-02 23:19:03 +0100

    BlogOG

--- blogog/app/dashboard/page.tsx
+++ blogog/app/dashboard/page.tsx
@@ -5,31 +5,49 @@ import React from "react";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { TPost } from "../types";
+import axios from "axios";
+
+const getMyPosts = async ():Promise<TPost [] | null> => {
+  try {
+    const res = await axios.get(`${process.env.NEXTAUTH_URL}/api/posts`);
+    const postsData = await res.data;
+    return postsData;
+  } catch (error) {
+    console.log(error);
+  }
+  return null
+};
 
 const page = async () => {
   const session = await getServerSession(authOptions)
-  console.log(session)
+
   if(!session){
     redirect("/signin")
   }
+
+  const posts = await getMyPosts()
+  
+  const myPosts = posts?.filter(post => post.authorEmail === session.user?.email)
+  console.log(myPosts)
   return (
     <div className="">
       <h1 className="title-page">My Posts : {session?.user?.name}</h1>
       <div className="mx-1 p-4 lg:px-10">
-        {postsData?.length > 0 ? (
-          postsData.map((item) => (
+        {myPosts?.length > 0 ? (
+          myPosts.map((post) => (
             <Post
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              content={item.content}
-              author={item.author}
-              category={item.category}
-              datepublished={item.datepublished}
-              links={item.links || []}
-              thumbnail={item.thumbnail}
-              authoremail={"contact@og93.com"}
-            />
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                content={post.content}
+                author={post.author.name}
+                category={post.catName}
+                datepublished={post.createdAt}
+                links={post.links || []}
+                thumbnail={post.imageUrl}
+                authoremail={post.authorEmail}
+              />
           ))
         ) : (
           <div className="pt-6">No Posts Available.{" "}

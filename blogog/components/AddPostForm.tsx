bf2d0d5b5fcb18ddba79680341c8f10b76a50425
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-05 01:20:30 +0100

    BlogOG

--- blogog/components/AddPostForm.tsx
+++ blogog/components/AddPostForm.tsx
@@ -11,6 +11,7 @@ import { useRouter } from "next/navigation";
 import { FaRegImages } from "react-icons/fa";
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
+import { FcRemoveImage } from "react-icons/fc";
 
 const AddPostForm = () => {
   const [links, setLinks] = useState<string[]>([]);
@@ -48,7 +49,9 @@ const AddPostForm = () => {
     setLinks((prev) => prev.filter((_, i) => i !== index));
   };
 
-
+  const removeImage = async (e:React.FormEvent) => {
+    e.preventDefault()
+  }
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -136,25 +139,41 @@ const AddPostForm = () => {
         <CldUploadWidget
           uploadPreset="zpjvrpbk"
           onSuccess={(result) => {
-            setPublicId(result?.info?.public_id as string)
-            setImageUrl(result?.info?.url as string)
+            setPublicId(result?.info?.public_id as string);
+            setImageUrl(result?.info?.url as string);
           }}
         >
-          {({open}) => {
+          {({ open }) => {
             return (
-              <div onClick={()=> open()}
-              className="relative flex flex-col justify-center items-center gap-2 hover:scale-105 duration-300 bg-sky-50 h-28 w-full my-2 text-gray-500 font-semibold text-xs rounded-xl border border-dashed cursor-pointer">
-            <FaRegImages size={24} />
-            Upload Image for the post
-            {imageUrl && (
-          <Image src={imageUrl} fill alt={title} className="rounded-2xl absolute object-cover inset-0"/>
-        )}
-          </div>
-            )
+              <div
+                onClick={() => open()}
+                className="relative flex flex-col justify-center items-center gap-2 hover:scale-105 duration-300 bg-sky-50 h-28 w-full my-2 text-gray-500 font-semibold text-xs rounded-xl border border-dashed cursor-pointer"
+              >
+                <FaRegImages size={24} />
+                Upload Image for the post
+                {imageUrl && (
+                  <Image
+                    src={imageUrl}
+                    fill
+                    alt={title}
+                    className="rounded-xl absolute object-cover inset-0"
+                  />
+                )}
+              </div>
+            );
           }}
         </CldUploadWidget>
-        
-        <div className="mb-4">
+        {publicId && (
+          <div
+            className="flex items-center gap-2 pb-5 cursor-pointer text-red-600 text-sm font-medium "
+            onClick={removeImage}
+          >
+            <FcRemoveImage size={24} className="hover:scale-105" />
+            Remove Image
+          </div>
+        )}
+
+        <div className="my-4">
           <select
             onChange={(e) => setSelectedCategory(e.target.value)}
             className="block appearance-none w-full bg-white border border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"

c5ee4e74a1af39f028a26871225146ba1305f56e
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-05 23:37:03 +0100

    CRUD

--- app/addTodo/page.jsx
+++ app/addTodo/page.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 
-const page = () => {
+const AddTodoPage = () => {
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
@@ -24,18 +24,18 @@ const page = () => {
     return (
         <>
             <h1 className='text-xl font-bold border-b pb-3 text-slate-800'>Add New Todo</h1>
-            <form class="w-full mt-5 px-2">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            <form className="w-full mt-5 px-2">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                     Title :
                 </label>
                 <input onChange={(e) => setTitle(e.target.value)} class="appearance-none block w-full bg-gray-100 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="input-title" type="text" placeholder="add title here" />
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                     Description :
                 </label>
                 <input onChange={(e) => setDescription(e.target.value)} class="appearance-none block w-full bg-gray-100 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="input-title" type="text" placeholder="add description here" />
                 <button
                     onClick={() => addTodo(title, description)}
-                    class="w-full bg-slate-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    className="w-full bg-slate-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                     Add
                 </button>
             </form>
@@ -43,4 +43,4 @@ const page = () => {
     )
 }
 
-export default page
+export default AddTodoPage
